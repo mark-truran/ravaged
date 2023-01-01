1a3440b39ebd2c650042d0825f24c4bd5ae712a0
Mark Truran <dev@marktruran.net>
2023-01-01 14:50:49 +0000

    Added router libs and landing page

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Alert
-        color="success"
-        onDismiss={function onDismiss() { return alert("Alert dismissed!") }}
-      >
-        <span>
-          <span className="font-medium">
-            Success!
-          </span>
-          {' '}React + Tailwind + Flowbite are working OK
-        </span>
-      </Alert>
+      
     </div>
   );
 }
